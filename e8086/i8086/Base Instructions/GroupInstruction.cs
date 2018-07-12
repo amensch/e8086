80c4be6b1399d1f1f3c4989eb73ff30fecbf1933
amensch <adam@menschwi.com>
2018-07-12 14:35:06 -0500

    Added GroupInstruction class

---
+++ e8086/i8086/Base Instructions/GroupInstruction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDS.e8086
+{
+    public abstract class GroupInstruction : TwoByteInstruction
+    {
+        public GroupInstruction(byte opCode, byte SecondByte, IExecutionUnit eu, IBus bus) : base (opCode, eu, bus)
+        {
+            secondByte = new AddressMode(SecondByte);
+        }
+
+        protected override void PreProcessing()
+        {
+            // do nothing - base class calls Bus.NextIP() which we don't want!
+        }
+    }
+}
